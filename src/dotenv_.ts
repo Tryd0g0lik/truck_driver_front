f4535dec2b51745e7c1fd39e01297ad6498b3564
Denis <work80@mail.ru>
2025-09-16 16:47:25 +0700

    [master]: pause

--- src/dotenv_.ts
+++ src/dotenv_.ts
@@ -2,6 +2,13 @@ const YnAPIKey = process.env.YANDEX_DEVELOPE_API_KEY || "";
 
 export default {"YANDEX_DEVELOPE_API_KEY": YnAPIKey}
 
-Читать 
+// Читать 
 
-https://yandex.ru/maps-api/docs/js-api/common/connection/typescript.html#sposob-2
+// https://yandex.ru/maps-api/docs/js-api/common/connection/typescript.html#sposob-2
+
+
+// ПЕременные карты
+// status 
+// lat: ? Lng:
+// date
+//  time
