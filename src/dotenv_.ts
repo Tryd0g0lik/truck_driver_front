5c260b2075d25d8ec4e4f2f92a85587357ecc9eb
Denis <work80@mail.ru>
2025-09-16 16:44:40 +0700

    [master]: pouse

---
+++ src/dotenv_.ts
@@ -0,0 +1,7 @@
+const YnAPIKey = process.env.YANDEX_DEVELOPE_API_KEY || "";
+
+export default {"YANDEX_DEVELOPE_API_KEY": YnAPIKey}
+
+Читать 
+
+https://yandex.ru/maps-api/docs/js-api/common/connection/typescript.html#sposob-2
